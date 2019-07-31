9bff9760e6c47072d9809570150d7f0c30d15ee2
sophiagable <sophiagable@gmail.com>
2019-07-31 15:30:33 +0800

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from 'angularfire2/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyCRUDApp';
+
+  constructor(private afs: AngularFirestore){
+    console.log(afs);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+  // Your web app's Firebase configuration
+  var config = {
+    apiKey: "AIzaSyA2XMuthBqYb7qDwqusk_KF3BG0GDQ0u7M",
+    authDomain: "mycrudapp-32d50.firebaseapp.com",
+    databaseURL: "https://mycrudapp-32d50.firebaseio.com",
+    projectId: "mycrudapp-32d50",
+    storageBucket: "",
+    messagingSenderId: "784983823900",
+    appId: "1:784983823900:web:1b7653689eb47433"
+  };
+  // Initialize Firebase
+  // firebase.initializeApp(config);
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
