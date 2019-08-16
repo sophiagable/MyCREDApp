34ab2fc8de4452e413c31493016ed1278a7b1fe8
sophiagable <sophiagable@gmail.com>
2019-08-16 17:12:16 +0800

    84-add user 尚未完成，先上傳

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template:`<users></users>`,
+  template:`<router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'MyCRUDApp';
 
-  constructor(private afs: AngularFirestore){
-    console.log(afs);
-  }
+
 }
